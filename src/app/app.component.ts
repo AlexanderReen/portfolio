2ef006fea69ed7ffc6123c004e4454d6f24e3c54
Alexander Reen <alexanderreen@gmail.com>
2021-04-03 01:15:37 +0200

    add extra escape slash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Alexander's Portfolio';
+  title = 'Alexander\'s Portfolio';
 }
